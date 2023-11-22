8f3cbf84ed64362b6581fe5f29c1229b083bc774
Sergio <sergiodanielxd2004@gmail.com>
2023-11-21 22:55:41 -0600

    nueva version

--- app.js
+++ app.js
@@ -6,24 +6,25 @@ import pg from "pg";
 const port = 3000;
 const app = express();
 
-const db = new pg.Client({
-    user: "postgres", 
-    host: "localhost",
-    database: "Ferreteria",
-    password: "26032015",
-    port: 5432
-});
-db.connect();
-
+var nombre;
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = new pg.Client({
+    user: "postgres", 
+    host: "localhost",
+    database: "Fer3B",
+    password: "123456",
+    port: 5432
+});
+db.connect();
 
 const admin = "user@gmail.com";
 
 app.get("/", (req,res) =>{
     res.render("index.ejs");
-    console.log(db.query("SELECT * FROM cliente"));
+
 });
 
 app.get("/register", (req,res) =>{
@@ -35,15 +36,21 @@ app.get("/login", (req,res) =>{
 });
 
 app.get("/home", (req, res) =>{
-    res.render("home.ejs");
+    res.render("home.ejs", {nombre:nombre});
 });
 
 app.get("/factura", (req, res) =>{
     res.render("factura.ejs");
 });
 
-app.get("/compra", (req, res) =>{
-    res.render("compra.ejs");
+app.get("/compra", async (req, res) =>{
+    const listaProducto = await db.query("SELECT * from producto");
+    
+    let listaProducto2 = [];
+    for(let i = 0; i < listaProducto.rowCount; i++){
+        listaProducto2.push(listaProducto.rows[i]);
+    }
+    res.render("compra.ejs", {listaProducto2:listaProducto2});
 });
 
 app.get("/carrito", (req, res) =>{
@@ -59,25 +66,61 @@ app.get("/articulos", (req,res) =>{
 })
 
 app.post("/register", async (req, res) =>{
-    const nombre=req.body.nombre;
+     nombre=req.body.nombre;
     const apellido=req.body.apellido;
     const correo=req.body.correo;
     const contra=req.body.contra;
 
     if(req.body.correo === admin){
-        res.render("home_admin");  
+        res.render("home_admin.ejs");  
     }
 
     else{
         try {
-            await db.query("INSERT INTO cliente(nombres) VALUES($1)", [nombre]);
+            await db.query("INSERT INTO usuario(nombres, apellidos, correo, contra) VALUES($1, $2, $3, $4)", [nombre, apellido, correo, contra]);
         } catch (err) {
             console.log(err);
         }
-        res.render("home.ejs");
+        res.render("home.ejs", {nombre:nombre});
     }
    
 });
+
+app.post("/login", (req, res) =>{
+   const correo=req.body.correo;
+   const contra=req.body.contra;
+
+   if(req.body.correo === admin){
+       res.render("home_admin.ejs");  
+   }
+
+   else{
+        res.redirect("/login");
+   }
+  
+});
+
+app.post("/articulos", async (req, res) =>{
+    const nombreProducto=req.body.nombreProducto;
+    const descripcion =req.body.descripcion;
+    const precio =req.body.precio;
+    const codigoEscaneo=req.body.codigoEscaneo;
+
+    if(req.body.correo === admin){
+        res.render("home_admin.ejs");  
+    }
+
+    else{
+        try {
+            await db.query("INSERT INTO producto(nombreProducto, descripcion, precio, codigoEscaneo) VALUES($1, $2, $3, $4)", [nombreProducto, descripcion, precio, codigoEscaneo]);
+        } catch (err) {
+            console.log(err);
+        }
+       res.redirect("/articulos");
+    }
+   
+ });
+
 
 app.listen(port, ()=>{
     console.log("connected");
