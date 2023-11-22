c498d9a98e9a299280fb3aab50c6fad7316624db
CarolV005 <cvhahah005@gmail.com>
2023-11-22 01:06:41 -0600

    Version 1.2.0

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import pg from "pg";
 
 const port = 3000;
 const app = express();
+
+const db = new pg.Client({
+    user: "postgres", 
+    host: "localhost",
+    database: "Ferreteria",
+    password: "26032015",
+    port: 5432
+});
+db.connect();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const admin = "user@gmail.com";
+
 app.get("/", (req,res) =>{
     res.render("index.ejs");
+    console.log(db.query("SELECT * FROM cliente"));
 });
 
 app.get("/register", (req,res) =>{
@@ -37,8 +50,33 @@ app.get("/carrito", (req, res) =>{
     res.render("carrito.ejs");
 });
 
-app.post("/register", (req, res) =>{
-    res.render("home.ejs");
+app.get("/home_admin", (req,res) =>{
+    res.render("home_admin.ejs");
+});
+
+app.get("/articulos", (req,res) =>{
+    res.render("articulos.ejs");
+})
+
+app.post("/register", async (req, res) =>{
+    const nombre=req.body.nombre;
+    const apellido=req.body.apellido;
+    const correo=req.body.correo;
+    const contra=req.body.contra;
+
+    if(req.body.correo === admin){
+        res.render("home_admin");  
+    }
+
+    else{
+        try {
+            await db.query("INSERT INTO cliente(nombres) VALUES($1)", [nombre]);
+        } catch (err) {
+            console.log(err);
+        }
+        res.render("home.ejs");
+    }
+   
 });
 
 app.listen(port, ()=>{
